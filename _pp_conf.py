d7351806807bcbbcf5543dfddbeee7f67b5da8ea
Michael Mommert <michael.mommert@nau.edu>
2017-02-15 16:24:31 -0700

    _pp_conf PEP8 compliant

--- _pp_conf.py
+++ _pp_conf.py
@@ -9,55 +9,54 @@ import os
 import sys
 import logging
 import warnings
-
-# only import if Python3 is used
-if sys.version_info > (3,0):
-    from past.builtins import execfile
+from astropy import wcs
+from astropy.io import fits
+import numpy as np
 
 # import pipeline-specific modules
 from toolbox import *
 
+# only import if Python3 is used
+if sys.version_info > (3, 0):
+    from past.builtins import execfile
+
 
 def setup_diagnostics():
-    """This function sets the current data and diagnostics root
+    """
+    This function sets the current data and diagnostics root
     directories. The setup can and should be re-run in the current working
-    directory"""
+    directory
+    """
 
     # set up data and diagnostics directories
-    dataroot = os.getcwd()+'/'
-    diagroot = dataroot+'.diagnostics/'
+    dataroot = os.getcwd() + '/'
+    diagroot = dataroot + '.diagnostics/'
 
     # create diagnostics directory
     if not os.path.exists(diagroot):
-        os.mkdir(diagroot) 
+        os.mkdir(diagroot)
 
     # define diagnostic website filenames
     index_filename = 'diagnostics.html'
-    reg_filename   = '.diagnostics/' + 'registration.html'
-    cal_filename   = '.diagnostics/' + 'calibration.html'
-    res_filename   = '.diagnostics/' + 'results.html'
-
+    reg_filename = '.diagnostics/' + 'registration.html'
+    cal_filename = '.diagnostics/' + 'calibration.html'
+    res_filename = '.diagnostics/' + 'results.html'
 
     return (dataroot, diagroot, index_filename,
             reg_filename, cal_filename, res_filename)
 
-
-
-### suppress runtime and astropy warnings
-warnings.simplefilter(action = "ignore", category = RuntimeWarning)
-from astropy import wcs
-from astropy.io import fits
-import numpy
+# suppress runtime and astropy warnings
+warnings.simplefilter(action="ignore", category=RuntimeWarning)
 warnings.filterwarnings('ignore', category=wcs.FITSFixedWarning)
 warnings.filterwarnings('ignore', category=fits.column.VerifyWarning)
 warnings.filterwarnings('ignore', category=fits.card.VerifyWarning)
 # following warning gets cast by Gaia query: XXX.convert_unit_to(u.deg)
-warnings.filterwarnings('ignore', category=numpy.ma.core.MaskedArrayFutureWarning)
+warnings.filterwarnings('ignore', category=np.ma.core.MaskedArrayFutureWarning)
 warnings.filterwarnings('ignore', category=UserWarning)
 
-### read photometry pipeline root path from environment variable
+# read photometry pipeline root path from environment variable
 rootpath = os.environ.get('PHOTPIPEDIR')
-if rootpath == None:
+if rootpath is None:
     print('ERROR: PHOTPIPEDIR variable has not been set')
     sys.exit(0)
 
@@ -68,58 +67,49 @@ if rootpath == None:
 # implemented_telescopes= []
 # execfile(rootpath + '/setup/vatt4k.py')
 
-
-### potential FITS header keywords for looking up the instrument
+# potential FITS header keywords for looking up the instrument
 # any unique header keyword works as a potential identifier
 instrument_keys = ['INSTRUME', 'LCAMMOD']
 
 execfile(rootpath+'/setup/telescopes.py')
 
-
-#### set diagnostics root directory and logging file
+# set diagnostics root directory and logging file
 
 # master_diagroot is the directory where the pipeline has been started from
 # i.e., potentially, the root directory of all underlying data
 # this definition is outside setup_diagnostics(), as it is called only once
-diagnostics_summary     = os.getcwd()+'/summary.html'
-use_diagnostics_summary = False  # set this to True, if you want each 
-                                 # pp_run process to report into a 
-                                 # summary html catalog
-
+diagnostics_summary = os.getcwd() + '/summary.html'
+use_diagnostics_summary = False
+# set this to True, if you want each
+# pp_run process to report into a summary html catalog
 
 # setting up directory paths and logging file
 dataroot, diagroot, index_filename, \
     reg_filename, cal_filename, res_filename = setup_diagnostics()
 
-
-### logging setup
+# logging setup
 log_formatline = '%(filename)s: %(message)s [%(asctime)s]'
-log_level      = logging.DEBUG
-log_datefmt    = '%m/%d/%Y %H:%M:%S'
+log_level = logging.DEBUG
+log_datefmt = '%m/%d/%Y %H:%M:%S'
 log_filename = diagroot+'LOG'
 
 # start pp_process_idx counter (if using 'pp_run all')
 pp_process_idx = 0
 
-
-### available catalogs
+# available catalogs
 
 # list of available catalogs
 allcatalogs = ['URAT-1', '2MASS', 'SDSS-R9', 'APASS9', 'GAIA']
 
 # catalog magnitude systems
-allcatalogs_magsys = {'URAT-1'  : 'Vega',
-                      '2MASS'   : 'Vega',
-                      'SDSS-R9' : 'AB',
-                      'APASS9'  : 'Vega',
-                      'GAIA'    : 'Vega'}
+allcatalogs_magsys = {'URAT-1': 'Vega',
+                      '2MASS': 'Vega',
+                      'SDSS-R9': 'AB',
+                      'APASS9': 'Vega',
+                      'GAIA': 'Vega'}
 
-
-
-
-##### pipeline preferences 
+# ---- pipeline preferences
 # (if you don't know what you're doing, better don't mess around here)
-
 
 # minimum number of reference sources in astrometric reference catalog
 # in one of the frames (middle one of sequence); try other catalog
@@ -131,7 +121,6 @@ n_registration_repetitions = 2
 
 # minimum number of reference sources in photometric reference catalog
 min_sources_photometric_catalog = 3
-
 
 # SCAMP contrast criteria for a good fit
 scamp_as_contrast_limit = 2.5
@@ -150,8 +139,7 @@ fluxmargin_aprad = 0.05
 # photometric calibration
 minstars = 0.5
 
-
-#### support 
+# support
 
 # path to local variable star database file
 # make sure set this variable accordingly if you want to use the catalog
