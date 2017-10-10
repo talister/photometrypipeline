5bf30789c763c819cf935025df2a8b97a745360f
Michael Mommert <mommermi@gmail.com>
2017-10-10 10:07:11 -0700

    prioritize PANSTARRS for VATT4k

--- setup/telescopes.py
+++ setup/telescopes.py
@@ -85,7 +85,7 @@ vatt4k_param = {
 
     # default catalog settings
     'astrometry_catalogs'  : ['GAIA'], 
-    'photometry_catalogs'  : ['SDSS-R9', 'APASS9', 'PANSTARRS', '2MASS']
+    'photometry_catalogs'  : ['SDSS-R9', 'PANSTARRS', 'APASS9']
 }
 
 
