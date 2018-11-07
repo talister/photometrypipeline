c50f04fd3a10ea23f20e93106dfc42b8a61b7aa2
Michael Mommert <mommermiscience@gmail.com>
2018-11-07 13:25:24 -0700

    new LCOGT telescopes added

--- setup/telescopes.py
+++ setup/telescopes.py
@@ -2724,6 +2724,73 @@ lcospecfs01_param = {
 }
 
 
+# LCOGT, SINISTRO camera (SAAO, FL06)
+lcosinfl06_param = {
+    # telescope/instrument name
+    'telescope_instrument': 'LCOGT(SAAO)/SINISTRO',
+    'telescope_keyword': 'LCOSINFL06',      # telescope/instrument keyword
+    'observatory_code': 'K92',         # MPC observatory code
+    'secpix': (0.389, 0.389),  # pixel size (arcsec)
+    # before binning
+    'ext_coeff': 0.05,          # typical extinction coefficient
+
+
+    # image orientation preferences
+    'flipx': False,
+    'flipy': True,
+    'rotate': 0,
+
+    # instrument-specific FITS header keywords
+    'binning': ('CCDSUM#blank0', 'CCDSUM#blank1'),  # binning in x/y
+    'extent': ('NAXIS1', 'NAXIS2'),   # N_pixels in x/y
+    'ra': 'RA',  # telescope pointing, RA
+    'dec': 'DEC',  # telescope pointin, Dec
+    'radec_separator': ':',   # RA/Dec hms separator, use 'XXX'
+    # if already in degrees
+    'date_keyword': 'DATE-OBS',  # obs date/time
+    # keyword; use
+    # 'date|time' if
+    # separate
+    'obsmidtime_jd': 'MJD-OBS',  # obs midtime jd keyword
+    # (usually provided by
+    # pp_prepare
+    'object': 'OBJECT',  # object name keyword
+    'filter': 'FILTER',  # filter keyword
+    'filter_translations': {'gp': 'g', 'rp': 'r',
+                            'ip': 'i', 'zp': 'z', 'w': None},
+    # filtername translation dictionary
+    'exptime': 'EXPTIME',  # exposure time keyword (s)
+    'airmass': 'AIRMASS',  # airmass keyword
+
+    # source extractor settings
+    'source_minarea': 9,  # default sextractor source minimum N_pixels
+    'source_snr': 3,  # default sextractor source snr for registration
+    'aprad_default': 5,  # default aperture radius in px
+    'aprad_range': [2, 10],  # [minimum, maximum] aperture radius (px)
+    'sex-config-file': rootpath+'/setup/lcosin.sex',
+    'mask_file': {},
+    #                        mask files as a function of x,y binning
+
+    # registration settings (Scamp)
+    'scamp-config-file': rootpath+'/setup/lcosin.scamp',
+    'reg_max_mag': 18,
+    'reg_search_radius': 0.5,  # deg
+    'source_tolerance': 'high',
+
+    # swarp settings
+    'copy_keywords': ('TELESCOP,INSTRUME,FILTER,EXPTIME,OBJECT,' +
+                      'DATE-OBS,RA,DEC,SECPIX,AIRMASS,' +
+                      'TEL_KEYW,MIDTIMJD'),
+    #                         keywords to be copied in image
+    #                         combination using swarp
+    'swarp-config-file': rootpath+'/setup/vatt4k.swarp',
+
+    # default catalog settings
+    'astrometry_catalogs': ['GAIA'],
+    'photometry_catalogs': ['PANSTARRS', 'SDSS-R9', 'APASS9']
+}
+
+
 # Apache Point Observatory 3.5m, SPICAM
 arc35spicam_param = {
     'telescope_instrument': 'ARC35/SPICAM',  # telescope/instrument name
@@ -3270,6 +3337,7 @@ implemented_telescopes = ['VATT4K', 'DCTLMI', 'ARC35ARCTIC',
                           'KPNO4NEWF', 'UKIRTWFCAM', 'VLTFORS2',
                           'LOWELL42SITE', 'PLUTOPLATE', 'TCS15MUSCAT2',
                           'LCOSBIGKB78', 'ARC35SPICAM', 'LCOSINFL03',
+                          'LCOSINFL06',
                           'LCOSINFL16', 'LCOSINFL11', 'LCOSINFA15',
                           'LCOSPECFS01', 'P60OPT', 'P60SEDM', 'GMOSN',
                           'DFOSC', 'LONEOS']
@@ -3322,6 +3390,7 @@ instrument_identifiers = {'= "Vatt4k"':        'VATT4K',
                           'kb78': 'LCOSBIGKB78',
                           'spicam': 'ARC35SPICAM',
                           'fl03': 'LCOSINFL03',
+                          'fl06': 'LCOSINFL06',
                           'fl16': 'LCOSINFL16',
                           'fl11': 'LCOSINFL11',
                           'fa15': 'LCOSINFA15',
@@ -3372,6 +3441,7 @@ telescope_parameters = {'VATT4K':       vatt4k_param,
                         'LCOSBIGKB78': lcosbigkb78_param,
                         'ARC35SPICAM': arc35spicam_param,
                         'LCOSINFL03': lcosinfl03_param,
+                        'LCOSINFL06': lcosinfl06_param,
                         'LCOSINFL16': lcosinfl16_param,
                         'LCOSINFL11': lcosinfl11_param,
                         'LCOSINFA15': lcosinfa15_param,
